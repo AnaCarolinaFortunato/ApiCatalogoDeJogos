2a1a24f28ebe1945a9e515e3794d203dca930996
AnaCarolinaFortunato <anacarolinafortunatosantos@gmail.com>
2021-07-07 16:54:00 -0300

    Adicionar arquivos de projeto.

---
+++ ApiCatalogoDeJogos/Repositories/IJogoRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApiCatalogoDeJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoDeJogos.Repositories
+{
+    public class IJogoRepository
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+}
---
+++ ApiCatalogoDeJogos/ViewModel/JogoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoDeJogos.ViewModel
+{
+    public class JogoViewModel
+    {
+        public Guid Id { get;  set; }
+
+        public String Nome { get;  set; }
+
+        public String Produtora { get; set; }
+
+        public double Preco { get; set; }
+
+
+
+    }
+}
